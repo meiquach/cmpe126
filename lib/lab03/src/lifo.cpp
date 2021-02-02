a0fd316640c29416bcc2354ff2f356c2b3029bf0
Gokay Saldamli <gokay.saldamli@sjsu.edu>
2021-02-02 15:15:23 -0800

    add lib

---
+++ lib/lab03/src/lifo.cpp
@@ -0,0 +1,43 @@
+#include "lifo.h"
+
+namespace lab3{
+    lifo::lifo() {
+    //Reserve 100 spaces in lifo_storage
+    }
+
+    lifo::lifo(std::string input_string) {
+
+    }
+
+    lifo::lifo(const lifo &original) {
+
+    }
+
+    lifo::~lifo() {
+
+    }
+
+    lifo &lifo::operator=(const lifo &right) {
+        //return <#initializer#>;
+    }
+
+    bool lifo::is_empty(){
+        //return false;
+    }
+
+    int lifo::size(){
+        //return 0;
+    }
+
+    std::string lifo::top(){
+        //return std::__cxx11::string();
+    }
+
+    void lifo::push(std::string input) {
+
+    }
+
+    void lifo::pop() {
+
+    }
+}
---
+++ lib/lab05/src/linked_list.cpp
@@ -0,0 +1,59 @@
+#include <linked_list.h>
+namespace lab5 {
+    linked_list::linked_list() {
+
+    }
+
+    linked_list::linked_list(std::string &data) {
+
+    }
+
+    linked_list::linked_list(const linked_list &original) {
+
+    }
+
+    linked_list::~linked_list() {
+
+    }
+
+    linked_list &lab5::linked_list::operator=(const linked_list &RHS) {
+        //return <#initializer#>;
+    }
+
+
+    bool linked_list::isEmpty() const {
+        return false;
+    }
+
+    unsigned linked_list::listSize() const {
+        return 0;
+    }
+
+    void linked_list::insert(const std::string input, unsigned int location) {
+
+    }
+
+    void linked_list::append(const std::string input) {
+
+    }
+
+    void linked_list::remove(unsigned location) {
+
+    }
+
+    std::ostream& operator<<(std::ostream &stream, linked_list &RHS) {
+        return stream;
+    }
+
+    std::istream& operator>>(std::istream &stream, linked_list &RHS) {
+        return stream;
+    }
+
+    void linked_list::sort() {
+
+    }
+
+    std::string linked_list::get_value_at(unsigned location) {
+
+    }
+}
