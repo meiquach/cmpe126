9901640eec5c8bf70757c35b3e3e6daf3655af95
Gokay Saldamli <gokay.saldamli@sjsu.edu>
2021-02-02 15:03:31 -0800

    initial commit

---
+++ lib/lab03/src/fifo.cpp
@@ -0,0 +1,43 @@
+#include "fifo.h"
+
+namespace lab3{
+    fifo::fifo() {
+    //Reserve 100 spaces in fifo_storage
+    }
+
+    fifo::fifo(std::string input_string) {
+
+    }
+
+    fifo::fifo(const fifo &original) {
+
+    }
+
+    fifo::~fifo() {
+
+    }
+
+    fifo &fifo::operator=(const fifo &right) {
+        //return <#initializer#>;
+    }
+
+    bool fifo::is_empty(){
+        //return false;
+    }
+
+    int fifo::size(){
+        //return 0;
+    }
+
+    std::string fifo::top(){
+        //return std::__cxx11::string();
+    }
+
+    void fifo::enqueue(std::string input) {
+
+    }
+
+    void fifo::dequeue() {
+
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Thursday everyone!";
+    return 0;
+}
